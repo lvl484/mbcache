a9974940228c6e5be8c0243d239051a9785ad504
Maksym Bilokur <maksym.bilokur@gmail.com>
2020-02-24 10:26:50 +0200

    commented all db connections, c.cache initializated in main to avoid panic

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,27 +8,27 @@ import (
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbhost = "localhost"
-	dbport = 5432
-	dbuser = "postgres"
-	dbpass = "postgres"
-	dbname = "cachedb"
-)
+//const (
+//	dbhost = "localhost"
+//	dbport = 5432
+//	dbuser = "postgres"
+//	dbpass = "postgres"
+//	dbname = "cachedb"
+//)
 
 func main() {
+	c.cache = make(map[string]CacheValue)
+	//psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+	//	"password=%s dbname=%s sslmode=disable",
+	//	dbhost, dbport,
+	//	dbuser, dbpass, dbname)
 
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		dbhost, dbport,
-		dbuser, dbpass, dbname)
-
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-	getFromDB()
+	//db, err := sql.Open("postgres", psqlInfo)
+	//if err != nil {
+	// 	log.Println(err)
+	//}
+	//defer db.Close()
+	//getFromDB()
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -38,10 +36,10 @@ func main() {
 	}
 
 	defer close(queueCache)
-	go delTracker()
-	go queueTracker()
+	//go delTracker()
+	//go queueTracker()
 
-	err = http.ListenAndServe(":"+port, newRouter())
+	err := http.ListenAndServe(":"+port, newRouter())
 	if err != nil {
 		log.Println(err)
 	}
