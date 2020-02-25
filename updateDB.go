2264d9f72db885accd88c28422af85b4be1c5696
Maksym Bilokur <maksym.bilokur@gmail.com>
2020-02-25 18:43:45 +0200

    deleted not implemented funcs

--- updateDB.go
+++ updateDB.go
@@ -7,21 +7,6 @@ import (
 	"time"
 )
 
-//Create new cache in BD
-func updateDBCreate(data *JsonBodyValue) {
-
-}
-
-//update cache in DB
-func updateDBUpdate(data *JsonBodyValue) {
-
-}
-
-//delete cache from db
-func updateDBDelete(delKey string) {
-
-}
-
 //get all data to cache when server starts
 func getFromDB(db *sql.DB) {
 	rows, err := db.Query(
